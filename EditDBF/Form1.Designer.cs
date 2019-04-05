4e7c94b1dca7cc745df3c87aff89ccb0080b5cc6
xmamontov <fortr4sh@gmail.com>
2019-04-05 09:50:29 +0300

    edit code

--- EditDBF/Form1.Designer.cs
+++ EditDBF/Form1.Designer.cs
@@ -28,13 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnOpenDBF = new System.Windows.Forms.Button();
+            this.btnEditDBF = new System.Windows.Forms.Button();
+            this.btnSaveDBF = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBcountRows1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tBcountRows2 = new System.Windows.Forms.TextBox();
+            this.tBMIAC = new System.Windows.Forms.TextBox();
+            this.tBDesc = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(319, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(337, 367);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(690, 101);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(337, 367);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // btnOpenDBF
+            // 
+            this.btnOpenDBF.Location = new System.Drawing.Point(319, 58);
+            this.btnOpenDBF.Name = "btnOpenDBF";
+            this.btnOpenDBF.Size = new System.Drawing.Size(130, 23);
+            this.btnOpenDBF.TabIndex = 2;
+            this.btnOpenDBF.Text = "1. Выбрать файл";
+            this.btnOpenDBF.UseVisualStyleBackColor = true;
+            this.btnOpenDBF.Click += new System.EventHandler(this.btnOpenDBF_Click);
+            // 
+            // btnEditDBF
+            // 
+            this.btnEditDBF.Location = new System.Drawing.Point(618, 58);
+            this.btnEditDBF.Name = "btnEditDBF";
+            this.btnEditDBF.Size = new System.Drawing.Size(126, 23);
+            this.btnEditDBF.TabIndex = 3;
+            this.btnEditDBF.Text = "2. Обработать";
+            this.btnEditDBF.UseVisualStyleBackColor = true;
+            this.btnEditDBF.Click += new System.EventHandler(this.btnEditDBF_Click);
+            // 
+            // btnSaveDBF
+            // 
+            this.btnSaveDBF.Location = new System.Drawing.Point(879, 58);
+            this.btnSaveDBF.Name = "btnSaveDBF";
+            this.btnSaveDBF.Size = new System.Drawing.Size(148, 23);
+            this.btnSaveDBF.TabIndex = 4;
+            this.btnSaveDBF.Text = "3. Сохранить";
+            this.btnSaveDBF.UseVisualStyleBackColor = true;
+            this.btnSaveDBF.Click += new System.EventHandler(this.btnSaveDBF_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 484);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество записей:";
+            // 
+            // tBcountRows1
+            // 
+            this.tBcountRows1.BackColor = System.Drawing.SystemColors.Control;
+            this.tBcountRows1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBcountRows1.Location = new System.Drawing.Point(436, 484);
+            this.tBcountRows1.Name = "tBcountRows1";
+            this.tBcountRows1.Size = new System.Drawing.Size(100, 13);
+            this.tBcountRows1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(690, 484);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Обработано записей:";
+            // 
+            // tBcountRows2
+            // 
+            this.tBcountRows2.BackColor = System.Drawing.SystemColors.Control;
+            this.tBcountRows2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBcountRows2.Location = new System.Drawing.Point(812, 484);
+            this.tBcountRows2.Name = "tBcountRows2";
+            this.tBcountRows2.Size = new System.Drawing.Size(100, 13);
+            this.tBcountRows2.TabIndex = 8;
+            // 
+            // tBMIAC
+            // 
+            this.tBMIAC.BackColor = System.Drawing.SystemColors.Control;
+            this.tBMIAC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBMIAC.Location = new System.Drawing.Point(111, 25);
+            this.tBMIAC.Multiline = true;
+            this.tBMIAC.Name = "tBMIAC";
+            this.tBMIAC.Size = new System.Drawing.Size(187, 91);
+            this.tBMIAC.TabIndex = 9;
+            // 
+            // tBDesc
+            // 
+            this.tBDesc.BackColor = System.Drawing.SystemColors.Control;
+            this.tBDesc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBDesc.Location = new System.Drawing.Point(27, 122);
+            this.tBDesc.Multiline = true;
+            this.tBDesc.Name = "tBDesc";
+            this.tBDesc.Size = new System.Drawing.Size(262, 375);
+            this.tBDesc.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(93, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1063, 532);
+            this.Controls.Add(this.tBDesc);
+            this.Controls.Add(this.tBMIAC);
+            this.Controls.Add(this.tBcountRows2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tBcountRows1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveDBF);
+            this.Controls.Add(this.btnEditDBF);
+            this.Controls.Add(this.btnOpenDBF);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Обработка DBF-файла для ДЛО";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnOpenDBF;
+        private System.Windows.Forms.Button btnEditDBF;
+        private System.Windows.Forms.Button btnSaveDBF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBcountRows1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tBcountRows2;
+        private System.Windows.Forms.TextBox tBMIAC;
+        private System.Windows.Forms.TextBox tBDesc;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- EditDBF/Form1.cs
+++ EditDBF/Form1.cs
@@ -6,14 +6,240 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
 
 namespace EditDBF
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+
+            #region текстовое описание для логотипа и о работе программы
+            tBMIAC.Text = "Медицинский" +
+                Environment.NewLine +
+                "информационно-аналитический центр" +
+                Environment.NewLine +
+                "Калужской области";
+            tBDesc.Text = "Описание:" +
+                Environment.NewLine + Environment.NewLine +
+                "ТФОМС Калужской области присылает DBF-файл, который содержит сведения о полисах физических лиц. " +
+                "По умолчанию этот файл имеет название \"rnsu.DBF\" и лежит в папке DLO на диске C." +
+                Environment.NewLine + Environment.NewLine +
+                "В этом DBF-файле полис записан одной строкой и его нужно разделить на \"серию\" и \"номер\", чтобы затем отправить эти данные в сервис \"ДЛО\"." +
+                Environment.NewLine + Environment.NewLine +
+                "Программа делит полисы и создаёт новый файл." +
+                Environment.NewLine +
+                "Новый файл с названием \"SNILS_POLIS.DBF\" следует сохранить в папку DLO на диске C." +
+                Environment.NewLine + Environment.NewLine +
+                "Чтобы начать работу с программой, нажмите кнопку \"Выбрать файл\"." +
+                Environment.NewLine + Environment.NewLine +
+                "Если появилась ошибка, что \"Поставщик VFPOLEDB.1 " +
+                " не зарегистрирован на локальном компьютере\", то необходимо скачать и установить дополнительный файл. Ссылка - " +
+                Environment.NewLine +
+                "https://www.microsoft.com/en-us/download/details.aspx?id=14839" +
+                Environment.NewLine + Environment.NewLine +
+                "Если произошли другие ошибки, то просто перезапустите программу.";
+            #endregion
+        }
+
+        private void btnOpenDBF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDBF = new OpenFileDialog();
+            openDBF.Multiselect = false;
+            openDBF.DefaultExt = ".dbf";
+            openDBF.Filter = "Database files|*.dbf";
+
+            if (openDBF.ShowDialog() == DialogResult.OK)
+            {
+                string openDBFfileName;
+                if (openDBF.FileName.IndexOf(" ") > 0)
+                {
+                    MessageBox.Show("В пути к файлу содержатся пробелы!" + "\n" + 
+                    "Поэтому файл будет перемещён во временную директорию и только потом открыт!" + "\n" + 
+                    "Нажмите \"ОК\" и дождитесь открытия файла!", "Предупреждение");
+                    Directory.CreateDirectory("C:\\Windows\\Temp\\CopyDBF\\");
+                    openDBFfileName = "C:\\Windows\\Temp\\CopyDBF\\" + DateTime.Now.ToShortDateString().Replace(".", "") + ".dbf";
+
+                    try
+                    {
+                        File.Copy(openDBF.FileName, openDBFfileName, true);
+                    }
+                    catch (IOException copyStatus)
+                    {
+                        MessageBox.Show("oops... Что-то пошло не так" + "\n" + copyStatus.Message);
+                    }
+                }
+                else
+                {
+                    openDBFfileName = openDBF.FileName;
+                }
+
+                string oleConnSettings = String.Format("Provider=VFPOLEDB.1;Data Source={0};", openDBFfileName);
+
+                OleDbConnection oleConn = new OleDbConnection(oleConnSettings);
+                oleConn.Open();
+
+                string query = "SELECT * FROM " + Path.GetFileNameWithoutExtension(openDBFfileName);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, oleConn);
+                DataSet dsDBFopen = new DataSet();
+                adapter.Fill(dsDBFopen);
+                dataGridView1.DataSource = dsDBFopen.Tables[0];
+                tBcountRows1.Text = dataGridView1.Rows.Count.ToString();
+
+                oleConn.Close();
+
+                MessageBox.Show("Теперь нажмите кнопку \"Обработать\"", "Сообщение");
+            }
+        }
+
+        private void btnEditDBF_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0) MessageBox.Show("Обрабатывать нечего... " +
+                "Выберите корректный DBF-файл и запустите его обработку", "Предупреждение");
+            if (dataGridView1.Rows.Count > 0)
+            {
+                OleDbConnection oleConn = new OleDbConnection(String.Format("Provider=VFPOLEDB.1;Data Source={0};Extended Properties=dBASE IV;",
+                    Path.GetTempPath()));
+                oleConn.Open();
+                OleDbCommand oleCmd = oleConn.CreateCommand();
+                oleCmd.CommandText = "CREATE TABLE newTable (" +
+                "sn_pol c(20)," +
+                "ss c(20)," +
+                "ser_pol c(20)," +
+                "num_pol c(20))";
+                oleCmd.ExecuteNonQuery();
+
+                DataSet dsDBFnew = new DataSet();
+                DataTable newTable = new DataTable();
+
+                dsDBFnew.Tables.Add(newTable);
+
+                newTable.Columns.Add("SN_POL", typeof(string));
+                newTable.Columns.Add("SS", typeof(string));
+                newTable.Columns.Add("SER_POL", typeof(string));
+                newTable.Columns.Add("NUM_POL", typeof(string));
+
+                DataRow row;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    string POLIS = Convert.ToString(dataGridView1.Rows[i].Cells[0].Value);
+                    POLIS = POLIS.Trim();
+                    string SER_POL = "";
+                    string NUM_POL = "";
+                    int countSpaces = POLIS.Length - POLIS.Replace(" ", "").Length; // подсчитывает число пробелов в строке
+                    int indexOfNumb = POLIS.IndexOf("№");                           // есть ли знак номера в строке
+
+                    #region Группа условий
+                    // первое условие
+                    if (POLIS == "")
+                    {
+                        SER_POL = "0";
+                        NUM_POL = "0";
+                    }
+
+                    // второе условие
+                    if (POLIS.Length > 0 && countSpaces == 0)
+                    {
+                        SER_POL = "0";
+                        NUM_POL = POLIS;
+                    }
+
+                    // третье условие
+                    if (countSpaces > 0)
+                    {
+                        if (countSpaces == 1)
+                        {
+                            string[] myArray = POLIS.Split(' ');
+                            SER_POL = myArray[0];
+                            NUM_POL = myArray[1];
+                        }
+                        if (countSpaces == 1 && indexOfNumb == 0)
+                        {
+                            string[] myArray = POLIS.Split(' ');
+                            SER_POL = "0";
+                            NUM_POL = myArray[1];
+                        }
+                        if (countSpaces == 2 && indexOfNumb == 0)
+                        {
+                            string[] myArray = POLIS.Split(' ');
+                            SER_POL = myArray[1];
+                            NUM_POL = myArray[2];
+                        }
+                        if (countSpaces == 2 && indexOfNumb > 0 )
+                        {
+                            string[] myArray = POLIS.Split(' ');
+                            SER_POL = myArray[0];
+                            NUM_POL = myArray[2];
+                        }
+                        if (countSpaces > 2 && indexOfNumb > 0)
+                        {
+                            string[] myArray = POLIS.Replace(" № ", " ").Trim().Split(' ');
+                            SER_POL = myArray[1];
+                            NUM_POL = myArray[2];
+                        }
+                    }
+                    #endregion
+
+                    ///----- добавление записей в пустую Таблицу нового ДатаСета -----///
+                    row = newTable.NewRow();
+                    row["SN_POL"] = Convert.ToString(dataGridView1.Rows[i].Cells[0].Value);
+                    row["SS"] = Convert.ToString(dataGridView1.Rows[i].Cells[1].Value);
+                    row["SER_POL"] = SER_POL;
+                    row["NUM_POL"] = NUM_POL;
+                    newTable.Rows.Add(row);
+
+                    ///----- заполнение DBF-файла данными -----///
+                    oleCmd.CommandText = String.Format("INSERT INTO newTable (" +
+                    "[sn_pol], " +
+                    "[ss], " +
+                    "[ser_pol], " +
+                    "[num_pol]) VALUES ('{0}', '{1}', '{2}', '{3}')",
+                    Convert.ToString(dsDBFnew.Tables[0].Rows[i][0]),
+                    Convert.ToString(dsDBFnew.Tables[0].Rows[i][1]),
+                    Convert.ToString(dsDBFnew.Tables[0].Rows[i][2]),
+                    Convert.ToString(dsDBFnew.Tables[0].Rows[i][3]));
+                    oleCmd.ExecuteNonQuery();
+
+                }
+                oleConn.Close();
+
+                dataGridView2.DataSource = dsDBFnew.Tables[0];
+                tBcountRows2.Text = dataGridView2.Rows.Count.ToString();
+                MessageBox.Show("Файл успешно обработан!!!" + "\n" + "Чтобы сохранить новый файл, нажмите кнопку \"Сохранить\"", "Сообщение");
+            }
+
+        }
+
+        private void btnSaveDBF_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count == 0) MessageBox.Show("Выберите корректный DBF-файл " +
+                "и запустите его обработку, только потом его можно будет сохранить", "Предупреждение");
+            if (dataGridView1.Rows.Count > 0)
+            {
+                SaveFileDialog saveDBF = new SaveFileDialog();
+                saveDBF.Filter = "Database files|*.dbf";
+                saveDBF.Title = "Сохранить файл";
+                saveDBF.FileName = "SNILS_POLIS.dbf";
+                saveDBF.DefaultExt = ".dbf";
+
+                if (saveDBF.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.Copy(Path.GetTempPath().ToString() + "newTable.dbf", saveDBF.FileName, true);
+                        MessageBox.Show("Файл успешно сохранён! Можно закрыть программу!", "Сообщение");
+                    }
+                    catch (IOException copyStatus)
+                    {
+                        MessageBox.Show("oops..." + "\n" + copyStatus.Message, "Ошибка");
+                    }
+                }
+            }
         }
     }
 }
